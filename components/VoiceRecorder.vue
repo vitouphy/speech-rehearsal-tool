b06d48a0e7c2add524f752d22f0523508bdf5a87
Vitou Phy <phy_vitou@live.com>
2022-05-29 18:08:46 +0900

    refactor voice recording module

---
+++ components/VoiceRecorder.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <el-button v-if="!isRecording" @click="clickButton">
+      <i class="el-icon-microphone"></i>
+      <span>Record</span>
+    </el-button>
+    <div v-else style="display: flex; align-items: center; gap: 10px">
+      <el-button type="danger" @click="clickButton">
+        <i class="el-icon-close"></i>
+        <span>Stop</span>
+      </el-button>
+      <el-progress
+        type="circle"
+        status="exception"
+        :percentage="(this.recordingDuration / TOTAL_DURATION) * 100"
+        :show-text="false"
+        :width="30"
+        :stroke-width="5"
+      ></el-progress>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+const CONSTRAINTS = {
+  video: false,
+  audio: {
+    channelCount: 1,
+    echoCancellation: false,
+  },
+}
+
+export default Vue.extend({
+  props: ['onFinishRecording'],
+  watch: {},
+  data() {
+    return {
+      // Constants
+      TOTAL_DURATION: 30, 
+      // Recording data properties
+      recorder: null as any as MediaRecorder,
+      isRecording: false,
+      recordingDuration: 0,
+      // Timer and Stopwatch
+      timer: null as any as NodeJS.Timer,
+      recordingTimeout: null as any as NodeJS.Timeout,
+    };
+  },
+  methods: {
+
+    async clickButton() {
+      if (this.recorder != null && this.recorder.state == "recording") {
+        this.stopRecording();
+      } else {
+        this.startRecording();
+      }
+    },
+
+    startRecording() {
+      navigator.mediaDevices
+        .getUserMedia(CONSTRAINTS)
+        .then(this.onGettingMediaStream)
+        .catch(console.log);
+    },
+
+    stopRecording() {
+      this.isRecording = false;
+      this.recorder.stop();
+      this.recordingDuration = 0;
+      clearInterval(this.timer);
+      clearTimeout(this.recordingTimeout);
+    },
+
+    onGettingMediaStream(stream: MediaStream) {
+      this.recorder = new MediaRecorder(stream);
+      this.recorder.start();
+      this.timer = setInterval(() => this.recordingDuration++, 1000);
+      this.isRecording = true;
+
+      this.recorder.ondataavailable = (event: BlobEvent) => {
+        // Stop all audio tracks
+        for (let audioTrack of stream.getAudioTracks()) {
+          audioTrack.stop();
+        }
+        const audioPath = window.URL.createObjectURL(event.data);
+        const recordingBlob = event.data;
+        this.onFinishRecording(audioPath, recordingBlob);
+      };
+
+      // Record only TOTAL_DURATION seconds
+      this.recordingTimeout = setTimeout(() => this.stopRecording(), this.TOTAL_DURATION * 1000);
+    },
+
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,45 +3,17 @@
     <el-menu class="el-menu-demo" mode="horizontal">
       <el-menu-item index="1" style="float: none">Prepper</el-menu-item>
     </el-menu>
-
     <div style="width: 80%; left: 10%" v-loading="processingSpeech">
       <div style="height: 50px"></div>
-
-      <!-- Form -->
       <div>
-        <el-input
-          type="textarea"
-          :autosize="{ minRows: 2, maxRows: 4 }"
-          placeholder="Please input"
-          v-model="textarea"
-        >
+        <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 4 }" placeholder="Please input" v-model="textarea">
         </el-input>
-        <div>Phoneme From Audio: {{phonemeFromAudio}}</div>
-        <div>Phoneme From Text:  {{phonemeFromText}}</div>
+        <div>Phoneme From Audio: {{ phonemeFromAudio }}</div>
+        <div>Phoneme From Text: {{ phonemeFromText }}</div>
       </div>
-
       <div style="height: 50px"></div>
-
-      <!-- Action Buttons -->
       <div style="display: flex; gap: 10px">
-        <el-button v-if="!isRecording" @click="clickButton">
-          <i class="el-icon-microphone"></i>
-          <span>Record</span>
-        </el-button>
-        <div v-else style="display: flex; align-items: center; gap: 10px">
-          <el-button type="danger" @click="clickButton">
-            <i class="el-icon-close"></i>
-            <span>Stop</span>
-          </el-button>
-          <el-progress
-            type="circle"
-            status="exception"
-            :percentage="(this.recordingDuration / this.totalDuration) * 100"
-            :show-text="false"
-            :width="30"
-            :stroke-width="5"
-          ></el-progress>
-        </div>
+        <voice-recorder :onFinishRecording="this.onFinishRecording" />
         <audio-player :audioPath="this.audioPath"></audio-player>
       </div>
     </div>
@@ -49,106 +21,43 @@
 </template>
 
 <script lang="ts">
-import { resolve } from "path";
 import Vue from "vue";
 import AudioPlayer from "~/components/AudioPlayer.vue";
-// import BioMSA from 'biomsa'
-const { NWaligner, SWaligner } = require('seqalign');
-import TextAlign from "~/scripts/text-aligner"
+import TextAlign from "~/scripts/text-aligner";
 
 export default Vue.extend({
   components: { AudioPlayer },
   name: "IndexPage",
   data() {
     return {
-      timer: null as any as NodeJS.Timer,
-      recordingTimeout: null as any as NodeJS.Timeout,
-      recordingDuration: 0,
-      totalDuration: 30,
       textarea: "",
-      recorder: null as any as MediaRecorder,
-      isRecording: false,
       audioPath: null as any as string,
-      recordingBlob: null as any,
-      recordingPhoneme: '',
-      phonemeFromText: '',
-      phonemeFromAudio: '',
+      audioBlob: null as any,
+      phonemeFromText: "",
+      phonemeFromAudio: "",
       breakdown: [],
       processingSpeech: false,
-      constraints: {
-        video: false,
-        audio: {
-          channelCount: 1,
-          echoCancellation: false,
-        },
-      },
     };
   },
-  mounted() {
-    const text1 = "həloʊ wɜːld wɛlkʌm tuː maɪ vɛɹi nuː juːtuːb tʃænəl θɹiː".replace('ː', '')
-    const text2 = "hɛə ɛv riwənwoʊn kəm tumaɪrɛvɪnu ju tɪ ʧɪn"
-    const result = TextAlign.align(text1, text2)
-    console.log(result)
-  },
   methods: {
-    async clickButton() {
-      if (this.recorder != null && this.recorder.state == "recording") {
-        this.stopRecording();
-      } else {
-        this.startRecording();
-      }
-    },
-    stopRecording() {
-      this.isRecording = false;
-      this.recorder.stop();
-      this.recordingDuration = 0;
-      clearInterval(this.timer);
-      clearTimeout(this.recordingTimeout);
-    },
-    startRecording() {
-      navigator.mediaDevices
-        .getUserMedia(this.constraints)
-        .then(this.onGettingMediaStream)
-        .catch(console.log);
-    },
-    onGettingMediaStream(stream: MediaStream) {
-      this.isRecording = true;
-      this.recorder = new MediaRecorder(stream);
-      this.recorder.start();
-      this.timer = setInterval(() => this.recordingDuration++, 1000);
-      this.recorder.ondataavailable = (event: BlobEvent) => {
-        // stop all audio tracks
-        for (let audioTrack of stream.getAudioTracks()) {
-          audioTrack.stop();
-        }
-        this.audioPath = window.URL.createObjectURL(event.data);
-        this.recordingBlob = event.data;
-        this.startProcessing();
-      };
-
-      // Set recording in 30 seconds
-      this.recordingTimeout = setTimeout(() => {
-        this.stopRecording();
-      }, this.totalDuration * 1000);
+    onFinishRecording(audioPath: any, audioBlob: any) {
+      this.audioPath = audioPath;
+      this.audioBlob = audioBlob;
+      this.startProcessing();
     },
     startProcessing() {
       this.processingSpeech = true;
       this.getPhoneme()
       .then(phonemeFromAudio => {
-        // console.log('Getting phoneme from Audio: ', phonemeFromAudio)
         this.phonemeFromAudio = phonemeFromAudio;
       })
       .then(() => this.convertText2Phoneme(this.textarea))
       .then(phonemeFromTextRs => {
         this.phonemeFromText = phonemeFromTextRs['phoneme'];
         this.breakdown = phonemeFromTextRs['breakdown'];
-        // console.log('Getting phoneme from Text: ', phonemeFromTextRs['phoneme'])
-        // console.log('Breakdown: ', phonemeFromTextRs['breakdown'])
         return phonemeFromTextRs['breakdown']
       })
       .then(breakdown => {
-        console.log('Getting phoneme from Audio: ', this.phonemeFromAudio)
-        console.log('Getting phoneme from Text: ', this.phonemeFromText.replace(':', ''))
         const alignments = TextAlign.align(this.phonemeFromText.replace(':', ''), this.phonemeFromAudio)
         for (var i=0; i<breakdown.length; i++) {
           breakdown[i].push(alignments[i])
@@ -171,7 +80,7 @@ export default Vue.extend({
         let counter = 0;
         while (counter++ < 3) {
           try {
-            let response = await this.$axios.post('/api-phoneme', this.recordingBlob);
+            let response = await this.$axios.post('/api-phoneme', this.audioBlob);
             return resolve(response.data['text'])
           } catch(error) {
             await new Promise(resolve2 => setTimeout(resolve2, 1000*60))
@@ -179,7 +88,6 @@ export default Vue.extend({
         }
       });
     }
-  }
+  },
 });
-
 </script>
